b75f04b2f7270a3f4b7dbdfbc8fae12c9781b8a1
Sebastian Reconco <sebastianreconco@gmail.com>
2025-04-14 00:05:12 -0400

    registration + login API added

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post('/api/login', async (req, res, next) => {
     const { login, password } = req.body;
 
     const db = client.db();
-    const results = await db.collection('Users').find({ Login: login, Password: password }).toArray();
+    const results = await db.collection('Users').find({ Username: login, Password: password }).toArray();
 
 
     var id = -1;
@@ -47,6 +47,66 @@ app.post('/api/login', async (req, res, next) => {
 
     var ret = { id: id, firstName: fn, lastName: ln, error: '' };
     res.status(200).json(ret);
+});
+
+app.post('/api/register', async(req, res, next) => {
+    const { firstName, lastName, username, password } = req.body;
+
+    //validate there is input for all request arguements
+    if(!firstName || firstName.trim() === '' ||
+        !lastName || lastName.trim() === '' ||
+        !username || username.trim() === '' ||
+        !password || password.trim() === '')
+    {
+        return res.status(400).json({ UserId: -1, error:'Please ensure all fields are valid.'});
+    }
+
+    try
+    {
+        const db = client.db();
+
+        //check if username already in use
+        const existingUser = await db.collection('Users').findOne({ Login: username },
+            { collation: { locale: 'en', strength: 2 }}); //case-insensitive search (collation achieves this)
+        if(existingUser) //existing username found
+        {
+            return res.status(400).json({UserId: -1, error: 'Username already in use.'}) //error nums?
+        }
+
+        //generate UserId
+        const highestIdUser = await db.collection('Users')
+            .find() //finds every document in collection
+            .sort({ UserId: -1 }) //sorts documents in descending order (-1) based on UserId
+            .limit(1) //only provide the first document (1)
+            .toArray();
+
+        //new user's ID #
+        let newUserId = 1;
+        //if users exist
+        if(highestIdUser.length > 0)
+        {
+            newUserId = highestIdUser[0].UserId + 1; //increment the ID of current highest UserId for the new user
+        }
+
+        //create new user
+        const newUser =
+        {
+            UserId: newUserId,
+            FirstName: firstName,
+            LastName: lastName,
+            Login: username,
+            Password: password
+        }
+
+        //insert new user into mongo
+        await db.collection('Users').insertOne(newUser);
+        return res.status(200).json({ UserId:newUserId, FirstName: firstName,
+            LastName: lastName, Login: username, error: '' });
+    }
+    catch(e)
+    {
+        return res.status(500).json({ UserId: -1, error: 'Failed to register.'});
+    }
 });
 
 app.listen(5000); // start Node + Express server on port 5000
